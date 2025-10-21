8fbe61c7ba65ad1e197e2948348766ba8d02b0b5
AngelNikoloski <an30847@seeu.edu.mk>
2025-10-21 16:48:56 +0200

    22

--- Class_Exercises_Cameras/main.js
+++ Class_Exercises_Cameras/main.js
@@ -1,21 +1,21 @@
 import * as THREE from 'three';
 
-// Scene
+
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x202020);
 
-// Renderer
+
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-// Sizes
+
 const sizes = {
   width: window.innerWidth,
   height: window.innerHeight
 };
 
-// Orthographic Camera
+
 const aspectRatio = sizes.width / sizes.height;
 const camera = new THREE.OrthographicCamera(
   -1 * aspectRatio,  // left
@@ -28,18 +28,18 @@ const camera = new THREE.OrthographicCamera(
 camera.position.z = 3;
 scene.add(camera);
 
-// Cube
+
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshStandardMaterial({ color: 0x00ff88 });
 const cubeMesh = new THREE.Mesh(geometry, material);
 scene.add(cubeMesh);
 
-// Light
+
 const light = new THREE.DirectionalLight(0xffffff, 1);
 light.position.set(2, 2, 5);
 scene.add(light);
 
-// Animation loop
+
 function animate() {
   requestAnimationFrame(animate);
   cubeMesh.rotation.x += 0.05;
